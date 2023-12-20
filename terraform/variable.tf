3e0fcedb98a6e86828c6551d6c190e8003c56b32
Nguyen Viet Anh <93577916+vanhanh210@users.noreply.github.com>
2023-12-20 15:46:44 +0700

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -23,12 +23,12 @@ variable "instance_type" {
 variable "alert_email_id" {
   description = "Email id to send alerts to "
   type        = string
-  default     = "you-name@some-domain.com"
+  default     = "anh2101011@gmail.com"
 }
 
 ## Your repository url
 variable "repo_url" {
   description = "Repository url to clone into production machine"
   type        = string
-  default     = "https://github.com/josephmachado/data_engineering_project_template.git"
+  default     = "https://github.com/vanhanh210/Data-Engineering-Projects.git"
 }
